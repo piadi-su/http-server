6a7ae34d06ade907f76eefd425054c128aac6547
piadi-su <213212422+piadi-su@users.noreply.github.com>
2026-08-26 13:16:02 +0200

    add path traversal checker

--- src/main.c
+++ src/main.c
@@ -8,13 +8,45 @@
 #include <string.h>
 #include <sys/stat.h>
 #include <dirent.h>
+#include <limits.h>
 
 #define BUFFER_SIZE 6168
 
 #define DEFAULT_DIR "www"
 #define DEFAULT_PORT 8080
 
-// TODO: put default port and directory + use args and get files
+
+//path traversal check
+int is_safe_path(const char *base_dir, const char *req_path, char *safe_path, size_t safe_len) {
+    char raw_path[PATH_MAX];
+    char resolved_base[PATH_MAX];
+    char resolved_file[PATH_MAX];
+
+    if (realpath(base_dir, resolved_base) == NULL) {
+        return 0; 
+    }
+
+    if (strcmp(req_path, "/") == 0) {
+        snprintf(raw_path, sizeof(raw_path), "%s/index.html", base_dir);
+    } else {
+        snprintf(raw_path, sizeof(raw_path), "%s%s", base_dir, req_path);
+    }
+
+    if (realpath(raw_path, resolved_file) == NULL) {
+        return 0; 
+    }
+
+    size_t base_len = strlen(resolved_base);
+    if (strncmp(resolved_base, resolved_file, base_len) != 0) {
+        return 0; 
+    }
+
+    strncpy(safe_path, resolved_file, safe_len - 1);
+    safe_path[safe_len - 1] = '\0';
+
+    return 1; 
+}
+
 
 const char* get_file_info(const char* path)
 {
@@ -47,13 +79,10 @@ int main(int argc, char *argv[])
 
 
 	int port = DEFAULT_PORT;
-	char directory[512] = DEFAULT_DIR;
+	char directory[512] = {0};
+	strncpy(directory, DEFAULT_DIR, sizeof(directory) - 1);
+	directory[sizeof(directory) -1] = '\0';
 
-
-	if(argc == 1){
-		printf("\ndo ./file -h to fort app usage\n\n");
-		return EXIT_FAILURE;
-	}
 
 	if(argc > 1 && strcmp(argv[1], "-h")== 0){
 		print_usage();
@@ -61,7 +90,7 @@ int main(int argc, char *argv[])
 	}
 	
 
-	for(int i = 0; i < argc; i++)
+	for(int i = 1; i < argc; i++)
 	{
 		if(strcmp(argv[i], "-p") == 0){
 			if (i + 1 < argc) {
@@ -106,9 +135,6 @@ int main(int argc, char *argv[])
 		}
 
 	}
-
-
-	//da cambiare
 
 	int server_sock;
 	struct sockaddr_in address;
@@ -157,7 +183,7 @@ int main(int argc, char *argv[])
 	}
 
 	printf("[+] waiting for connection on http://localhost:%d...\n",port);
-	printf("[+] root dir: %s", directory);
+	printf("[+] root dir: %s\n", directory);
 
 	//select stuff
 	
@@ -187,10 +213,8 @@ int main(int argc, char *argv[])
 		{
 			if(FD_ISSET(i, &ready_socket)){
 
-				// Caso 1: Nuova connessione in ingresso sul server
-				if(i == server_sock)// questo vuol dire che nel socket del server principale c'é una richiesta di connessione
+				if(i == server_sock)
 				{
-					//servono solo per accept
 					struct sockaddr_in client_address;
 					socklen_t client_addr_len = sizeof(client_address);
 
@@ -238,46 +262,81 @@ int main(int argc, char *argv[])
 						char path[512];
 						
 						//sscanf do the parsing for Es. GET /index.html
-						sscanf(buffer, "%s %s", method, path);
-
+						sscanf(buffer, "%15s %511s", method, path);
 						printf("[+] %s request at: %s\n", method, path);
 
 						//we are olnly going to allow the GET request
 						if(strcmp(method, "GET") == 0)
 						{
 
-							//html body on the browser
-							// da cambiare con  file reading 
-							// const char *html_body = 
-							// 	"<!DOCTYPE html>"
-							// 	"<html>"
-							// 	"<head><title>Mio Server C</title></head>"
-							// 	"<body style='font-family: sans-serif; text-align: center; margin-top: 50px;'>"
-							// 	"  <h1 style='color: #2b5797;'>HTML Servito con Successo dal Server C!</h1>"
-							// 	"  <p>Hai effettuato una richiesta <b>GET</b> sulla rotta: <i>%s</i></p>"
-							// 	"</body>"
-							// 	"</html>";
-							//
-							
-							//buffer fort the http response
-							char http_response[BUFFER_SIZE * 2];
 
-							snprintf(http_response, sizeof(http_response), 
-								"HTTP/1.1 200 OK\r\n"
-								"Content-Type: text/html; charset=UTF-8\r\n"
-								"Content-Length: %zu\r\n" 
-								"Connection: close\r\n"
-								"\r\n"                   
-								"%s"
-								,strlen(html_body), html_body);
+							char safe_file_path[1024] = {0};
 
-							//send the response back to the client
-							send(i,http_response, strlen(http_response), 0);
-							
+							if(!is_safe_path(directory, path, safe_file_path, sizeof(safe_file_path)))
+							{
+								fprintf(stderr,"[!] error: the file path doesn't exist\n");
+
+								const char *body = "<h1>404 File Not Found</h1>";
+								char not_found[512];
+
+								snprintf(not_found, sizeof(not_found),
+										"HTTP/1.1 404 Not Found\r\n"
+										"Content-Type: text/html\r\n"
+										"Content-Length: %zu\r\n"   
+										"Connection: close\r\n"
+										"\r\n"
+										"%s",
+										strlen(body), body);
+
+								send(i, not_found, strlen(not_found), 0);
+
+							}
+							else
+							{
+
+								FILE *file = fopen(safe_file_path, "rb");
+
+								if(file != NULL){
+									fseek(file ,0,SEEK_END);
+									long file_size = ftell(file);
+									fseek(file ,0,SEEK_SET);
+
+
+
+									//HTTP RESPONSE//
+
+									//buffer fort the http response
+									char http_header[BUFFER_SIZE];
+
+									snprintf(http_header, sizeof(http_header), 
+											"HTTP/1.1 200 OK\r\n"
+											"Content-Type: %s\r\n"
+											"Content-Length: %ld\r\n" 
+											"Connection: close\r\n"
+											"\r\n"                   
+											,get_file_info(safe_file_path),file_size);
+
+									//send the header first
+									send(i,http_header, strlen(http_header), 0);
+
+									char GET_file_buffer[BUFFER_SIZE];
+
+									ssize_t read_bytes;
+
+									while((read_bytes = fread(GET_file_buffer, 1, sizeof(GET_file_buffer), file)) > 0)
+									{
+										send(i, GET_file_buffer, read_bytes, 0);
+									}
+									fclose(file);
+								}
+								else {
+									//if the file is valid but the server can't open it
+                                    const char *error_500 = "HTTP/1.1 500 Internal Server Error\r\nContent-Length: 0\r\nConnection: close\r\n\r\n";
+                                    send(i, error_500, strlen(error_500), 0);
+                                }
+							}
 						}
-
-						else
-						{
+						else{
 							//since only GET is allow if the requesst
 							//methon is something else this will show
 
@@ -296,14 +355,11 @@ int main(int argc, char *argv[])
 
 		}
 		
-		
 
 
 	}
 
 	
-
-
 
 	close(server_sock);
 	return EXIT_SUCCESS;
